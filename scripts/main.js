5fdaf0ff82c4c1bb8cf74acddf3edd6835cfe0fd
marniheijnis <58250084+marniheijnis@users.noreply.github.com>
2020-07-08 00:09:48 +0200

    theme
    created a theme and added the picture to animate

--- scripts/main.js
+++ scripts/main.js
@@ -21,5 +21,46 @@ function clock() {
 
 setInterval(clock,100);
 
+var date = new Date();
+var day = date.getDate() + "-" + (date.getMonth() +1) + "-" + date.getFullYear();
 
+var month = new Array();
+	month [0] = "January";
+	month [1] = "February";
+	month [2] = "March";
+	month [3] = "April";
+	month [4] = "May";
+	month [5] = "June";
+	month [6] = "July";
+	month [7] = "August";
+	month [8] = "September";
+	month [9] = "October";
+	month [10] = "November";
+	month [11] = "December";
 
+var d = new Date();
+var n = month[d.getMonth()];
+document.getElementById("months").innerHTML = n;
+
+var month = new Array();
+	day [0] = "Monday";
+	day [1] = "Tuesday";
+	day [2] = "Wednesday";
+	day [3] = "Thursday";
+	day [4] = "Friday";
+	day [5] = "Saturday";
+	day [6] = "Sunday";
+
+var s = new Date();
+document.getElementById("days").innerHTML = s;
+document.querySelector(".date").innerHTML= day;
+
+function background() {
+	var time = new Date();
+	var hours = time.getHours(); 
+	if (hours > 9 && hours < 23) {
+		document.body.style.backgroundColor = '#87CEFA';
+	} else {
+		document.body.style.backgroundColor = '#fff399';
+	}
+}
